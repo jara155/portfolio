58ba0ba4d7d0fa33a8acb2293d0fe0ed6dd747ad
Jája <74739998+jara155@users.noreply.github.com>
2024-12-18 19:40:34 +0100

    Improved meta data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -279,6 +279,7 @@
 		align-items: center;
 		justify-content: center;
 		width: 100vw;
+		margin-bottom: 6rem;
 		background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(255 255 255 / 0.015)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e");
 	}
 
@@ -336,7 +337,6 @@
 		header {
 			display: flex;
 			height: 100vh;
-			margin-bottom: 5rem;
 		}
 
 		.name {
